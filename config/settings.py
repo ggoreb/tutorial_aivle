9e769be400f362a5c4b341a2bbff0cff438cbe29
GGoReb <seorab@naver.com>
2022-01-25 09:02:00 +0900

    static 폴더 추가, 자바스크립트 클릭 이벤트, 유효성 검사 정규식 예시

--- config/settings.py
+++ config/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 LOGIN_REDIRECT_URL = '/member/login/'
 LOGOUT_REDIRECT_URL = '/member/login/'
